30720ddee727e8a11f9a4c05c3f91ef5af6fc977
tejaswini414 <saitejaswini414@gmail.com>
2025-12-04 09:43:47 +0530

    Update condition.py

---
+++ condition.py
@@ -0,0 +1,9 @@
+temp = 60
+if(temp>80):
+  print("it's hot outside")
+  print("stay inside")
+elif(temp<60):
+  print("it's cold outside")
+  print("wear warm clothes")
+else:
+  print("enjoy outside")
