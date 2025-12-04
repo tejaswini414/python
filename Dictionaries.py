0771661afd09ef8ed2038afb70293eb4b03b5c0d
tejaswini414 <saitejaswini414@gmail.com>
2025-12-04 18:38:31 +0530

    update dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,5 @@
+acronyms = {}
+acronyms['LOL'] = 'Laugh out loud'
+acronyms['BRB'] = 'Be right Back'
+acronyms['IDK'] = "I don't know"
+print(acronyms)
--- Loopwithrange.py
+++ Loopwithrange.py
@@ -1,7 +1,7 @@
 total = 0
 expenses = []
 num_expenses = int(input("enter number of expenses:"))
-for i in range(7):
+for i in range(num_expenses):
     expenses.append(float(input("enter expenses:")))
 total = sum(expenses)
 print("Expense so far:$", total, sep = '')
