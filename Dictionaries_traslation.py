8c373bdc6f951b51c95e6f7ccb876ff232c76840
tejaswini414 <saitejaswini414@gmail.com>
2025-12-04 18:49:58 +0530

    Update translation

---
+++ Dictionaries_traslation.py
@@ -0,0 +1,6 @@
+acronyms = {'LOL': 'Laugh out loud', 'BRB': 'Be right Back', 'IDK': "I don't know"}
+print(acronyms)
+
+sentense = 'IDK' + "what happened" + 'BRB'
+translation = acronyms['IDK'] + " what happened " + acronyms['BRB']
+print(translation)  # Output: I don't know what happened Be right Back
