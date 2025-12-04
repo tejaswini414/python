25923da3d133fa2164e2d3d12fc26072cbf62c28
tejaswini414 <saitejaswini414@gmail.com>
2025-12-04 12:26:48 +0530

    Update expenses

---
+++ expenses.py
@@ -0,0 +1,7 @@
+expenses = [10.3, 3, 15, 7.2, 20]
+sum =0
+for x in expenses:
+    sum = sum + x
+    total = sum
+    print("Expense so far:$", sum, sep = '')
+print("total expenses: $", total, sep = '')
