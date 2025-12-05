2505580e824944e1a73e2a5b41d4391eca9c629b
tejaswini414 <saitejaswini414@gmail.com>
2025-12-05 17:36:06 +0530

    Update python

---
+++ Function.py
@@ -0,0 +1,7 @@
+def greeting(name):
+    print("hello ", name)
+
+name1 =input("enter your name:")
+greeting(name1)
+name2 =input("enter your another name:")
+greeting(name2) 
---
+++ Function_addition.py
@@ -0,0 +1,8 @@
+def addition(a,b):
+    return a+b
+
+num1 = int(input("enter first number:"))
+num2 = int(input("enter second number:"))
+result = addition(num1,num2)
+print("the addition is:", result)
+           
