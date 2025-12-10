9bf18fd4357776790fdce32ba63e6cc3498bb568
tejaswini414 <saitejaswini414@gmail.com>
2025-12-10 19:35:42 +0530

    u

--- Function_addition.py
+++ Function_addition.py
@@ -5,5 +5,4 @@ num1 = int(input('Enter first number:'))
 num2 = int(input('Enter second number:'))
 result = addition(num1,num2)
 print("The sum is:", result)
-           # End of Function_addition.py
-# Function_addition.py
+           
